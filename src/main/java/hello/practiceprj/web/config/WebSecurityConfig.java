c2bfedda0e8671fd38a3a8b2d8d4f28402e41a00
Home_PC <hsp4567@naver.com>
2021-08-07 15:36:51 +0900

    무중단 배포 Setting

--- src/main/java/hello/practiceprj/web/config/WebSecurityConfig.java
+++ src/main/java/hello/practiceprj/web/config/WebSecurityConfig.java
@@ -31,7 +31,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                    .antMatchers("/","/board/list","/signup","/profile").permitAll()
+                    .antMatchers("/","/board/list","/signup","/profile","/actuator/health").permitAll()
                     .anyRequest().authenticated()
                     .and()
                 .formLogin()
