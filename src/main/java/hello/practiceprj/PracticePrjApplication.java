19f7b19cc93947b192e6b6818abe1547f998c97d
Home_PC <hsp4567@naver.com>
2021-08-07 18:38:26 +0900

    Revert "무중단 배포 Setting"
    This reverts commit fde424050064ac8eb2a357b0f06ed79e60422f16.
    
    # Conflicts:
    #	build.gradle
    #	src/main/resources/application.properties

--- src/main/java/hello/practiceprj/PracticePrjApplication.java
+++ src/main/java/hello/practiceprj/PracticePrjApplication.java
@@ -4,20 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.config.ConfigDataResourceNotFoundException;
 
 @MapperScan(value = {"hello.practiceprj.mapper"})
 @SpringBootApplication
 public class PracticePrjApplication {
 
-	public static final String APPLICATION_LOCATIONS_WIN = "spring.config.location=classpath:application.properties,C:/Users/user/real-application.yml";
-	public static final String APPLICATION_LOCATIONS_EC2 = "spring.config.location=classpath:application.properties,/app/config/SpringMVCBoard/real-application.yml";
+//	public static final String APPLICATION_LOCATIONS = "spring.config.location=classpath:application.properties,C:/Users/user/real-application.yml";
+	public static final String APPLICATION_LOCATIONS = "spring.config.location=classpath:application.properties,/app/config/SpringMVCBoard/real-application.yml";
 
 	public static void main(String[] args) {
-		try {
-			new SpringApplicationBuilder(PracticePrjApplication.class).properties(APPLICATION_LOCATIONS_WIN).run(args);
-		}catch(ConfigDataResourceNotFoundException e){
-			new SpringApplicationBuilder(PracticePrjApplication.class).properties(APPLICATION_LOCATIONS_EC2).run(args);
-		}
+		new SpringApplicationBuilder(PracticePrjApplication.class).properties(APPLICATION_LOCATIONS).run(args);
 	}
 }
