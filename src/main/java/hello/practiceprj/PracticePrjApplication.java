0377818b978cdddf25003efaf037aadf79093077
Home_PC <hsp4567@naver.com>
2021-08-08 19:41:08 +0900

    회원가입시 이메일 인증 추가

--- src/main/java/hello/practiceprj/PracticePrjApplication.java
+++ src/main/java/hello/practiceprj/PracticePrjApplication.java
@@ -6,6 +6,8 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.config.ConfigDataResourceNotFoundException;
 
+import java.io.File;
+
 @MapperScan(value = {"hello.practiceprj.mapper"})
 @SpringBootApplication
 public class PracticePrjApplication {
@@ -14,10 +16,11 @@ public class PracticePrjApplication {
 	public static final String APPLICATION_LOCATIONS_EC2 = "spring.config.location=classpath:application.properties,/app/config/SpringMVCBoard/real-application.yml";
 
 	public static void main(String[] args) {
-		try{
-			new SpringApplicationBuilder(PracticePrjApplication.class).properties(APPLICATION_LOCATIONS_EC2).run(args);
-		}catch(ConfigDataResourceNotFoundException e){
+		File f = new File("C://Users/user/real-application.yml");
+		if (f.exists()) {
 			new SpringApplicationBuilder(PracticePrjApplication.class).properties(APPLICATION_LOCATIONS_WIN).run(args);
+		} else {
+			new SpringApplicationBuilder(PracticePrjApplication.class).properties(APPLICATION_LOCATIONS_EC2).run(args);
 		}
 	}
 }
